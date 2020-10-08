b67df5e6f83b381dd37060a84109586d7c8d67b3
Ivan Herrera (Bellkhen) <71898783+Ivan-Herrera-Garcia@users.noreply.github.com>
2020-10-07 21:23:44 -0500

    Update Datos.vb

--- Datos.vb
+++ Datos.vb
@@ -2,21 +2,23 @@
 
 Public Class Datos
     Private conexion As SqlConnection = New SqlConnection("Data Source=LAPTOP-GKDBUDPG\MSSQLSERVER01;Initial Catalog=Escuela;Integrated Security=True")
+    'Para la conexion se necesita el nombre del servidor'
+    'Tambien se necesitara tener existente la base de datos a usar, en este caso sera 'Escuela'
     Function Abrir_Conexion() As Boolean
-        conexion.Open()
+        conexion.Open() 'Abre conexion
         Return True
     End Function
     Function Cerrar_Conexion() As Boolean
-        conexion.Close()
+        conexion.Close() 'Cierra conexion
         Return True
     End Function
 
 
     '----------------------------------------------------------------------------------------------------------------'
     'Conexiones'
-    Function Traductor(ByVal instruccion As String)
+    Function Traductor(ByVal instruccion As String) 'Envia una cadena en la cual va la sentencia SQL
         Try
-            Dim comando As SqlCommand = New SqlCommand(instruccion, conexion)
+            Dim comando As SqlCommand = New SqlCommand(instruccion, conexion) 'La instruccion y la conexion para poder abrir un canal a SQL
             comando.ExecuteNonQuery()
             Return True
         Catch
