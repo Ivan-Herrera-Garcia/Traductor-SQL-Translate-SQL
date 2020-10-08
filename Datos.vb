02f77ca445b625a3333b14c36a57dbe09f7736ee
Ivan Herrera (Bellkhen) <71898783+Ivan-Herrera-Garcia@users.noreply.github.com>
2020-10-07 21:19:16 -0500

    TRASLATE-TRADUCTOR
    Clase en la cual se abre la conexión con SQL y se encuentra el método para realizar sentencias SQL. Planeo después agregar un método para visualizar los cambios y búsquedas.

---
+++ Datos.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Datos
+    Private conexion As SqlConnection = New SqlConnection("Data Source=LAPTOP-GKDBUDPG\MSSQLSERVER01;Initial Catalog=Escuela;Integrated Security=True")
+    Function Abrir_Conexion() As Boolean
+        conexion.Open()
+        Return True
+    End Function
+    Function Cerrar_Conexion() As Boolean
+        conexion.Close()
+        Return True
+    End Function
+
+
+    '----------------------------------------------------------------------------------------------------------------'
+    'Conexiones'
+    Function Traductor(ByVal instruccion As String)
+        Try
+            Dim comando As SqlCommand = New SqlCommand(instruccion, conexion)
+            comando.ExecuteNonQuery()
+            Return True
+        Catch
+            Return False
+        End Try
+    End Function
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,47 @@
+﻿Imports System.Drawing.Imaging
+Imports System.Globalization
+Imports System.Reflection.Emit
+Imports System.Text
+Imports System.Timers
+
+Public Class Form1
+
+    Private Sub SalirToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Close()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim cadena As String = "La base de datos cuenta con los siguentes atributos: nombre_completo varchar(30), edad int, exo Char(3) null default 'M', id Char(8) primary key, carrera varchar(30)"
+
+
+        MessageBox.Show(cadena, "Informacion", MessageBoxButtons.OK)
+
+
+    End Sub
+
+    Private Sub ToolStripLabel1_Click(sender As Object, e As EventArgs) Handles ToolStripLabel1.Click
+
+        Dim Ejecutar As Datos = New Datos()
+        Ejecutar.Abrir_Conexion()
+        Dim a As Boolean
+        a = Ejecutar.Traductor(RichTextBox1.Text)
+        If (a = True) Then
+            DialogResult = MessageBox.Show("Comando ejecutado correctamente, ¿desea eliminar los datos en el campo?", "Informacion", MessageBoxButtons.YesNo)
+            If (DialogResult.Yes) Then
+                RichTextBox1.Clear()
+            End If
+        Else
+            MessageBox.Show("Error en el comando", "Informacion", MessageBoxButtons.OK)
+        End If
+        Ejecutar.Cerrar_Conexion()
+    End Sub
+
+    Private Sub ToolStripLabel2_Click(sender As Object, e As EventArgs) Handles ToolStripLabel2.Click
+        MessageBox.Show("CONSULTE AL DESARROLLADOR", "Ayuda", MessageBoxButtons.OK)
+    End Sub
+
+    Private Sub ToolStripLabel3_Click(sender As Object, e As EventArgs) Handles ToolStripLabel3.Click
+        Close()
+
+    End Sub
+End Class
